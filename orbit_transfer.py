e427c9c6d264ecc8cfef676301cb05cb3cee6c08
tamfr <ejm29@case.edu>
2015-07-28 18:00:33 -0400

    Added orbit_transfer.py.

---
+++ orbit_transfer.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 28 17:46:15 2015
+
+@author: Mott
+"""
+
+from config import Earth, Mars
+from planets import planet
+
+Earth = planet(Earth)
+Mars = planet(Mars)
