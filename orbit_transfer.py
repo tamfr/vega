e74fe4828cdcc28091bca456eb2078bcd22b7cf9
tamfr <ejm29@case.edu>
2015-07-28 22:20:41 -0400

    Created orbital_tools module.

--- orbit_transfer.py
+++ orbit_transfer.py
@@ -4,6 +4,10 @@ Created on Tue Jul 28 17:46:15 2015
 
 @author: Mott
 """
+# Important Remarks: 
+# 1. J2000 Used to Establish All Constants
+# 2. For Analysis, Earth's Perihelion is the Epoch (JDN 2455565.547 [i.e. CE 2011 January 04 01:07:40.8 UT  Tuesday])
+# 3. Inclination and Orbital Precession are Neglected
 
 from config import Earth, Mars
 from planets import planet
---
+++ orbital_tools.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 28 22:09:52 2015
+
+@author: Mott
+"""
+
+import numpy as np
+
+def eccentric_anomaly(e,M):    
+    
+    # Iteration Loop to Find Eccentric Anomaly
+    
+    E0 = np.pi/3 # Initilize Eccentric Anomaly for Loop
+    E1 = -np.pi/2 # Initilize Eccentric Anomaly for Loop
+
+    while abs(E1/E0 - 1) > 0.0005: # Condition On Which to Run Loop
+        if E1 > 2*np.pi:
+            E1 = E1 - 2*np.pi
+        elif E1 < 0:
+            E1 = E1 + np.pi
+        else:
+            E0 = E1
+            E1 = E0 - (E0 - e*np.sin(E0) - M) / (1 - e*np.cos(E0)) # Newton's Root Finding Method
+        
+    
+    fM1 = 2*np.arctan(((1 + e) / (1 - e))**(1/2)*np.tan(E1/2)) # Mars True Anomaly for Min Hohmann Transfer
+    
+    # Test to Make Sure True Anomaly is Between 0 and 360 Degrees
+    
+    if fM1 < 0: 
+        fM1 = fM1 + 2*np.pi
+    elif fM1 > 2*np.pi:
+        fM1 = fM1 - 2*np.pi
+    
